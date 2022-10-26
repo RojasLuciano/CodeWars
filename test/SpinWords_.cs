66fe182b626070a9aec82d919fbf94385bdd9c52
lucianorojas <lucianorojas@uala.com>
2022-10-25 23:29:29 -0300

    Primera subida

---
+++ test/SpinWords_.cs
@@ -0,0 +1,29 @@
+﻿namespace Clases;
+public static class PracticeClass
+{
+    public static string SpinWords(string sentence)
+    {
+        string[] arr = sentence.Split(" ");
+        var output = new List<string>();
+      
+        foreach (var item in arr)
+        {
+            if (item.Length <= 4)
+            {
+                output.Add(item);
+                continue;
+            }
+            var wordArray = item.ToCharArray();
+            var current = string.Empty;
+            for (int i = item.Length - 1; i >= 0; i--)
+            {
+                current += wordArray[i];
+            }
+            output.Add(current);
+        }
+        return string.Join(" ", output);
+    }
+
+}
+
+
---
+++ test1/SpinWords__.cs
@@ -0,0 +1,48 @@
+﻿using Clases;
+namespace UnitTest;
+
+
+[TestClass]
+public class UnitTest1
+{
+
+
+    [TestMethod]
+    public void Test1()
+    {
+        Assert.AreEqual("emocleW", PracticeClass.SpinWords("Welcome"));
+    }
+
+    [TestMethod]
+    public void Test2()
+    {
+        Assert.AreEqual("Hey wollef sroirraw", PracticeClass.SpinWords("Hey fellow warriors"));
+    }
+
+    [TestMethod]
+    public void Test3()
+    {
+        Assert.AreEqual("This is a test", PracticeClass.SpinWords("This is a test"));
+    }
+
+    [TestMethod]
+    public void Test4()
+    {
+        Assert.AreEqual("This is rehtona test", PracticeClass.SpinWords("This is another test"));
+    }
+
+    [TestMethod]
+    public void Test5()
+    {
+        Assert.AreEqual("You are tsomla to the last test", PracticeClass.SpinWords("You are almost to the last test"));
+    }
+
+    [TestMethod]
+    public void Test6()
+    {
+        Assert.AreEqual("Just gniddik ereht is llits one more", PracticeClass.SpinWords("Just kidding there is still one more"));
+    }
+
+
+
+}
